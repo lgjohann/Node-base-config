2182c6f1dce2986e091145f3531a13d8224d1e47
Guilherme Johann <guilherme4695@gmail.com>
2024-03-08 16:04:39 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+
+app.set('views', path.resolve((__dirname, 'src', 'views')));
+app.set('view engine', 'ejs');
+
+
+app.use(routes);
+
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`);
+    console.log('link: http://127.0.0.1:3000/');
+})
